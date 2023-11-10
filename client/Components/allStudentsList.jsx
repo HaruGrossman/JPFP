cec21ecc5e01b22e373eb54e2b3246b27c168b9c
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-10 15:05:01 -0600

    Create Components and Store folders

---
+++ client/Components/allStudentsList.jsx
@@ -0,0 +1,3 @@
+// returns how we want our student list to appear on the page
+//// list- per student:image, name, delete button
+// matches '/students' url
---
+++ client/Components/singleStudentDetail.jsx
@@ -0,0 +1,3 @@
+// returns how we want our single student details to appear on the page
+//// image, firstName, lastName, studentEmail, studentGPA, and studentCampus
+// matches '/students/:studentId' url
