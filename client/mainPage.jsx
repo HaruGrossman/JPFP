b59dedfec7631eb4295b826106b9d9627ca414f7
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-10 17:13:30 -0600

    Create mainPage

---
+++ client/mainPage.jsx
@@ -0,0 +1,10 @@
+import AllStudentList from "./Components/allStudentsList";
+import NavigationBar from "./Components/navigationBar";
+
+function MainPage() {
+    return (
+        NavigationBar,
+        AllStudentList
+    )
+}
+export default MainPage
