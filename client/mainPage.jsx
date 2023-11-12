823d527e657571deca1af8acf904db586dd3f43a
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-12 10:39:04 -0600

    install vite@latest

--- client/mainPage.jsx
+++ client/mainPage.jsx
@@ -1,4 +1,5 @@
 import AllStudentList from "./Components/allStudentsList";
+import Footer from "./Components/footer";
 import NavigationBar from "./Components/navigationBar";
 
 function MainPage() {
